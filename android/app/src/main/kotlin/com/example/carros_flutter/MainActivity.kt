d2d489424715e38f826cc650caa481d67e99964d
pedrorv_cit <pedrorv@ciandt.com>
2020-10-26 17:04:09 -0300

    Designe tela Login básica

---
+++ android/app/src/main/kotlin/com/example/carros_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.carros_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final _tLogin = TextEditingController();
+  final _tSenha = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Carros"),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView(
+        children: <Widget>[
+          _text("Login", "Digite o login", controller: _tLogin),
+          SizedBox(height: 10),
+          _text("Senha", "Digite a senha", password: true, controller: _tSenha),
+          SizedBox(height: 20),
+          _button("Login", _onClickLogin),
+        ],
+      ),
+    );
+  }
+
+  _onClickLogin() {
+    String login = _tLogin.text;
+    String senha = _tSenha.text;
+
+    print("Login: $login, Senha: $senha");
+  }
+
+  _text(String label, String hint,
+      {bool password = false, TextEditingController controller}) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      style: TextStyle(
+        fontSize: 25,
+        color: Colors.blue,
+      ),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(
+          fontSize: 25,
+          color: Colors.grey,
+        ),
+        hintText: hint,
+        hintStyle: TextStyle(
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
+
+  _button(String text, Function onPressed) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
