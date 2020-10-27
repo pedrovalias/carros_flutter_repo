efd8ce6c7bb70ede021c74ba3fc4bd68ed613344
pedrovalias <pedro.valias@hotmail.com>
2020-10-26 22:55:05 -0300

    Comunicacao parcial com webservice login

---
+++ lib/pages/login_api.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+// declara a biblioteca e atribui uma variavel para chamar
+import 'package:http/http.dart' as http;
+
+// classe simples que vai encapsular a lógica para chamar o webservice
+class LoginApi {
+  // Metodo estatico que vai retornar um boolean
+  static Future<bool> login(String login, String senha) async {
+    var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
+
+    // Configuracao
+    Map<String, String> headers = {"Content-Type": "application/json"};
+
+    Map parametros = {"username": login, "password": senha, headers: headers};
+
+    // Converte o hashmap para string
+    String s = json.encode(parametros);
+    print("> $s");
+
+    // Retorna a resposta do post no webservice
+    var response = await http.post(url,
+        // passa a string para o body
+        body: s,
+        headers: headers);
+
+    // Imprime o status code da resposta do webservice
+    print('Response status: ${response.statusCode}');
+    // Imprime o JASON de resposta
+    print('Response body: ${response.body}');
+
+    Map mapResponse = json.decode(response.body);
+
+    String nome = mapResponse["nome"];
+    String email = mapResponse["email"];
+
+    print("Nome: $nome");
+    print("Email: $email");
+
+    return true;
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,29 @@
+import 'package:carros_flutter/pages/home_page.dart';
+import 'package:carros_flutter/pages/login_api.dart';
+import 'package:carros_flutter/pages/utils/nav.dart';
+import 'package:carros_flutter/widget/app_button.dart';
+import 'package:carros_flutter/widget/app_text.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  final _tLogin = TextEditingController();
-  final _tSenha = TextEditingController();
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _tLogin = TextEditingController(text: "pedroValias");
+
+  final _tSenha = TextEditingController(text: "123");
+
+  final _focusSenha = FocusNode();
+
+  // Chamado na inicializacao
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,64 +36,82 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView(
-        children: <Widget>[
-          _text("Login", "Digite o login", controller: _tLogin),
-          SizedBox(height: 10),
-          _text("Senha", "Digite a senha", password: true, controller: _tSenha),
-          SizedBox(height: 20),
-          _button("Login", _onClickLogin),
-        ],
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            AppText(
+              "Login",
+              "Digite o login",
+              controller: _tLogin,
+              validator: _validateLogin,
+              keyboardType: TextInputType.emailAddress,
+              nextFocus: _focusSenha,
+            ),
+            SizedBox(height: 10),
+            AppText(
+              "Senha",
+              "Digite a senha",
+              controller: _tSenha,
+              password: true,
+              validator: _validateSenha,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              focusNode: _focusSenha,
+            ),
+            SizedBox(height: 20),
+            AppButton(
+              "Login",
+              onPressed: _onClickLogin,
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  _onClickLogin() {
+  _onClickLogin() async {
+    // bool _formOk = _formKey.currentState.validate();
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
     String login = _tLogin.text;
     String senha = _tSenha.text;
 
     print("Login: $login, Senha: $senha");
+
+    bool ok = await LoginApi.login(login, senha);
+
+    if (ok) {
+      push(context, HomePage());
+    } else {
+      print("Login incorreto");
+    }
   }
 
-  _text(String label, String hint,
-      {bool password = false, TextEditingController controller}) {
-    return TextFormField(
-      controller: controller,
-      obscureText: password,
-      style: TextStyle(
-        fontSize: 25,
-        color: Colors.blue,
-      ),
-      decoration: InputDecoration(
-        labelText: label,
-        labelStyle: TextStyle(
-          fontSize: 25,
-          color: Colors.grey,
-        ),
-        hintText: hint,
-        hintStyle: TextStyle(
-          fontSize: 16,
-        ),
-      ),
-    );
+  String _validateLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    }
+    return null;
   }
 
-  _button(String text, Function onPressed) {
-    return Container(
-      height: 46,
-      child: RaisedButton(
-        color: Colors.blue,
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-        onPressed: onPressed,
-      ),
-    );
+  String _validateSenha(String text) {
+    if (text.isEmpty) {
+      return "Digite a senha";
+    }
+    if (text.length < 3) {
+      return "A senha deve conter pelo menos 3 caracteres";
+    }
+    return null;
+  }
+
+  // Metodo responsavel por gerenciar a memoria das telas
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
